fe9afd74320066580309c2035cb01837ad60d673
ioelgomez2019 <ioelgomez2019@gmail.com>
2025-08-13 22:22:00 -0500

    finalizando conexxion a la base de datos

---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencias(this IservicesCollection services, IConfiguration configuration) {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+    }
+}
