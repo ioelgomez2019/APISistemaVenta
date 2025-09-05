b3b637236918f0c9b16a7ce22c643d796f25e783
ioelgomez2019 <ioelgomez2019@gmail.com>
2025-09-05 00:07:54 -0500

    57:39 sse completo la Ventadeservicioy su contrato

---
+++ SistemaVenta.BLL/Servicios/VentaService.cs
@@ -0,0 +1,102 @@
+﻿using SistemaVenta.BLL.Servicios.Contrato;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+using System.Globalization;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class VentaService: IVentaService
+    {
+        private readonly IVentaRepository _ventaRepositorio;
+        private readonly IGenericRepository<DetalleVenta> _detalleVentaRepositorio;
+        private readonly IMapper _mapper;
+
+        public  VentaService(IVentaRepository ventaRepository, IGenericRepository<DetalleVenta> detalleVentaRepositorio, IMapper mapper)
+        {
+            _ventaRepositorio = ventaRepository;
+            _detalleVentaRepositorio = detalleVentaRepositorio;
+            _mapper = mapper;
+        }
+        public async Task<VentaDTO> Registrar(VentaDTO modelo)
+        {
+            try
+            {
+                var ventaGenerada = await _ventaRepositorio.Registrar(_mapper.Map<Venta>(modelo));
+                if (ventaGenerada.IdVenta == 0)
+                    throw new TaskCanceledException("No se puede crear");
+                return _mapper.Map<VentaDTO>(ventaGenerada);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public async Task<List<VentaDTO>> Historial(string buscarPor, string numeroVenta, string fechaInicio, string fechaFin)
+        {
+            IQueryable<Venta> query = await _ventaRepositorio.Consultar();
+            var ListaResutado = new List<Venta>();
+            try {
+                if(buscarPor == "fecha")
+                {
+                    DateTime fech_inicio = DateTime.ParseExact(fechaInicio, "dd/MM/yyyy", new CultureInfo("es-PE"));
+                    DateTime fech_fin = DateTime.ParseExact(fechaFin,"dd/MM/yyyy", new CultureInfo("es-PE"));
+                    ListaResutado = await query.Where(v =>
+                        v.FechaRegistro.Value.Date >= fech_inicio &&
+                        v.FechaRegistro.Value.Date <= fech_fin
+                    ).Include(DetalleVenta=>DetalleVenta.DetalleVenta)
+                    .ThenInclude(p=>p.IdProductoNavigation)
+                    .ToListAsync();
+                }
+                else
+                {
+                    ListaResutado = await query.Where(v => v.NumeroDocumento == numeroVenta
+                    ).Include(DetalleVenta => DetalleVenta.DetalleVenta)
+                    .ThenInclude(p => p.IdProductoNavigation)
+                    .ToListAsync();
+                }
+                
+                
+            }catch {
+                throw;
+            }
+            return _mapper.Map<List<VentaDTO>>(ListaResutado);
+        }
+
+        
+
+        public async Task<List<ReporteDTO>> Reporte(string fechaInicio, string fechaFin)
+        {
+            IQueryable<DetalleVenta> query = await _detalleVentaRepositorio.Consultar();
+            var ListaResultado = new List<DetalleVenta>();
+            try
+            {
+                DateTime fech_inicio = DateTime.ParseExact(fechaInicio, "dd/MM/yyyy", new CultureInfo("es-PE"));
+                DateTime fech_fin = DateTime.ParseExact(fechaFin, "dd/MM/yyyy", new CultureInfo("es-PE"));
+
+                ListaResultado = await query
+                    .Include(p => p.IdProductoNavigation)
+                    .Include(V => V.IdVentaNavigation)
+                    .Where(dv =>
+                        dv.IdVentaNavigation.FechaRegistro.Value.Date >= fech_inicio.Date &&
+                        dv.IdVentaNavigation.FechaRegistro.Value.Date <= fech_fin.Date
+                    ).ToListAsync();
+
+            }
+            catch
+            {
+                throw;
+            }
+            return _mapper.Map<List<ReporteDTO>>(ListaResultado);
+        }
+    }
+}
